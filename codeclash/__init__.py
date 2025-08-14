d036aea20676eb0d1dc827fb34e77fa0ad42607b
Kilian Lieret <kilian.lieret@posteo.de>
2025-08-14 13:15:25 -0400

    Move viewer into package

--- codeclash/__init__.py
+++ codeclash/__init__.py
@@ -1,1 +1,9 @@
 __version__ = "0.0.0"
+
+# Optional viewer import - only if Flask dependencies are available
+try:
+    from . import viewer
+
+    __all__ = ["viewer"]
+except ImportError:
+    __all__ = []
