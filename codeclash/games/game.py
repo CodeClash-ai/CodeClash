0f3408c2d9a4c5a8cb86dbe5862c2da3cc483ab8
John Yang <byjohnyang@gmail.com>
2025-09-30 19:12:53 +0000

    Update glm-4-5, qwen 3 coder refs

--- codeclash/games/game.py
+++ codeclash/games/game.py
@@ -194,7 +194,7 @@ class CodeGame(ABC):
                 "PIP_PROGRESS_BAR": "off",
                 "TQDM_DISABLE": "1",
             },
-            container_timeout="3h",
+            container_timeout="10h",
             logger=self.logger,
             run_args=run_args,
         )
