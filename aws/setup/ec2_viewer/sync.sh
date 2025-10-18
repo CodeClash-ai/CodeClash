53b5a656a47c777a5124b9efe82f1c513d09cff1
Kilian Lieret <kilian.lieret@posteo.de>
2025-10-17 21:44:15 -0400

    Enh(viewer): Sync less often

--- aws/setup/ec2_viewer/sync.sh
+++ aws/setup/ec2_viewer/sync.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-SYNC_COMMAND="aws s3 sync s3://codeclash/logs/ $HOME/CodeClash/logs/ --exclude \"*/rounds/*\" --exclude \"*.tar.gz\" --delete"
+SYNC_COMMAND="nice -n 19 aws s3 sync s3://codeclash/logs/ $HOME/CodeClash/logs/ --exclude \"**/rounds/**\" --exclude \"**/*.tar.gz\" --delete"
 LOG_FILE="$HOME/sync.log"
 
 while true; do
     echo "$(date): Starting sync..." >> "$LOG_FILE"
     eval $SYNC_COMMAND >> "$LOG_FILE" 2>&1
     echo "$(date): Sync completed" >> "$LOG_FILE"
-    sleep 60
+    sleep 300
 done
